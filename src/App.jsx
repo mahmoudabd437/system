c3f50c51f7946429b0fa91829a80d0d2a9651f49
mahmoudabd437 <141342718+mahmoudabd437@users.noreply.github.com>
2026-07-22 15:11:03 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -33,9 +33,9 @@ function ShellPage({ children }) {
 function AppRoutes() {
   return (
     <Routes>
-      {/* <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} /> */}
+      <Route path="/signup" element={<SignupPage />} />
       <Route
         path="/dashboard"
         element={
